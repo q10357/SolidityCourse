bab1b342511c730ad9ac22382afb45e9735161d0
q10357 <isabel@chaparian.no>
2024-03-19 20:09:10 -0400

    deployed contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -5,12 +5,12 @@
 
 // 0.8.8 = Need exact version, ^0.8.8 = If you give me newer version, it's key
 // >=0.8.8 < 0.9.0 = equal or newer than v0.8.8, but not v0.0.9 or newer
-pragma solidity ^0.8.8; 
+pragma solidity 0.8.7;
 
 contract SimpleStorage {
     // 0 is default if no other value is set
     // dvs variable gets initialized to zero
-    uint256 favoriteNumber; 
+    uint256 favoriteNumber;
     //People public person = People({favoriteNumber: 2, name: "Isabel"});
 
     mapping(string => uint256) public nameToFavoriteNumber;
@@ -21,6 +21,7 @@ contract SimpleStorage {
 
     // uint256 public favoriteNumbersList;
     People[] public people;
+
     // index 0: 2, Isabel, 1: 3, Jon
 
     function store(uint256 _favoriteNumber) public virtual {
@@ -33,7 +34,7 @@ contract SimpleStorage {
         exception: If a gass calling function (non view/pure) calls 
         a view or pure function - only then will it cost gas    
     */
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
@@ -41,8 +42,8 @@ contract SimpleStorage {
     // calldata: temporary variables that cant be redefined
     // memory: same but can be redefined
     // storage: permanent variables that can be modified
-    function addPerson(string memory _name, uint256 _favoriteNumber) public { 
-        people.push( People(_favoriteNumber, _name));
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
--- deploy.js
+++ deploy.js
@@ -1,5 +1,32 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+require("dotenv").config();
+
 // code for deploying our contract
-async function main() {}
+async function main() {
+  // either compile contracts
+  // in code
+  // or compile them seperately
+  const privateKey = process.env.GANACHE_PRIVATE_KEY;
+  const addr = process.env.PROVIDER_ADDRESS;
+  if (!privateKey || !addr) {
+    throw new Error("Please define GANACHE_PRIVATE_KEY in your .env file");
+  }
+
+  const provider = new ethers.JsonRpcProvider(addr);
+
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait");
+
+  const contract = await contractFactory.deploy({ gasPrice: 10000000000 });
+  const transactionReceipt = await contract.deploymentTransaction().wait(1);
+}
 
 main()
   .then(() => process.exit(0))
